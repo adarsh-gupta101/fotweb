019c5d68dfe9c256d88d6ac3d80c792ecfb241c3
Adarsh <adarshgupta2468@gmail.com>
2021-11-13 14:28:30 +0530

    test

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ dotenv.config();
 
 const app=express();
 
-
+app.get("/",(req,res)=>{res.send("yummy tomato")})
 app.use(express.json());
-mongoose.connect(process.env.conn,{useNewUrlParser:true,useUnifiedTopology:true},()=>{
+mongoose.connect("mongodb+srv://dbadminuser:hHmi3r1F90uQ0OAW@cluster0.qd9fx.mongodb.net/userdbreview?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true},()=>{
 
 
     console.log('connected to db');
@@ -16,6 +16,10 @@ mongoose.connect(process.env.conn,{useNewUrlParser:true,useUnifiedTopology:true}
 const datapage=require('./data.js');
 app.use("/king",datapage);
 
-app.listen(process.env.PORT || 4000,()=>{
+app.listen(process.env.PORT || 4000,(data,err)=>{
     console.log('AWW snap ');
+    if(err)
+    {
+        console.log(err);
+    }
 });
